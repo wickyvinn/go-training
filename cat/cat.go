e2678420a54a75457592d48b0a182e3f4f7a6576
Vicky Nguyen <vnguyen@fastly.com>
2017-07-27 15:40:26 -0700

    Add flags to cat.

--- cat/cat.go
+++ cat/cat.go
@@ -1,32 +1,44 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	count := flag.Bool("count", false, "print # of bytes")
+	stderr := flag.Bool("stderr", false, "print to stderr")
+
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
 		log.Fatalf("please provide a filename.")
 		return
 	}
-	filename := os.Args[1]
-	if err := writeToStdOut(filename); err != nil {
-		log.Fatalf("%s", err)
+	s, num, err := readfile(flag.Args()[0])
+	if err != nil {
+		log.Fatalf("error with reading file.")
 		return
 	}
 
-}
-
-func writeToStdOut(filename string) error {
-	if b, err := ioutil.ReadFile(filename); err != nil {
-		return err
+	if *stderr == true {
+		os.Stderr.WriteString(s)
 	} else {
-		s := string(b)
-		log.Println(s)
+		fmt.Println(s)
 	}
 
-	return nil
+	if *count == true {
+		fmt.Printf("# of bytes: %d\n", num)
+	}
+}
 
+func readfile(filename string) (string, int, error) {
+	if b, err := ioutil.ReadFile(filename); err != nil {
+		return "", 0, err
+	} else {
+		return string(b), len(b), nil
+	}
 }
